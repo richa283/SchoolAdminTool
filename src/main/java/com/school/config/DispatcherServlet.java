affddd7e843bbfb7aab9526a29032374fc42ae8e
RICHA JHA <jricha283@gmail.com>
2023-10-18 06:40:29 +0530

    Created a DispatcherServlet class that configures the Spring MVC Dispatcher Servlet. This class sets up the servlet mappings and references the SchoolConfig class as the servlet configuration.

--- src/main/java/com/school/config/DispatcherServlet.java
+++ src/main/java/com/school/config/DispatcherServlet.java
@@ -1,5 +1,24 @@
 package com.school.config;
 
-public class DispatcherServlet {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+public class DispatcherServlet extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses(){
+		Class[] configFiles = { SchoolConfig.class };
+		return configFiles;
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+		String[] mappings = { "/" };
+		return mappings;
+	}
+	
 }
