07e34b8688ca96100aba2ea1bdb43aefcce36cd1
RICHA JHA <jricha283@gmail.com>
2023-10-19 21:26:13 +0530

    Added School Config file

--- src/main/java/com/school/config/SchoolConfig.java
+++ src/main/java/com/school/config/SchoolConfig.java
@@ -1,5 +1,62 @@
 package com.school.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configurable
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.school" })
 public class SchoolConfig {
 
+	// Configuring View Resolver
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	// Configurin MySQL Database
+	@Bean
+	public DataSource datasource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/gjschool");
+		return dataSource;
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(datasource());
+		return jdbcTemplate;
+	}
+	
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver multipartResolver() {
+	    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+	   // size in bytes : - 2 MB file size
+	    multipartResolver.setMaxUploadSize(2097152);
+	    return multipartResolver;
+	}
+	
+	
+	
+	@Bean
+	public PasswordEncoder encoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
