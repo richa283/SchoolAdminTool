be67397873045d7185b06fe72bb6632db938f295
RICHA JHA <jricha283@gmail.com>
2023-10-27 14:18:21 +0530

    Map SQL ResultSet to FeesAmountDto in RemFeesMapper

--- src/main/java/com/school/mapper/RemFeesMapper.java
+++ src/main/java/com/school/mapper/RemFeesMapper.java
@@ -1,5 +1,19 @@
 package com.school.mapper;
 
-public class RemFeesMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import org.springframework.jdbc.core.RowMapper;
+
+import com.school.dto.FeesAmountDto;
+
+public class RemFeesMapper  implements RowMapper<FeesAmountDto>{
+
+	@Override
+	public FeesAmountDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+		FeesAmountDto fees= new FeesAmountDto();
+		fees.setScholarNumberOrName(rs.getString(1));
+		fees.setAmount(rs.getString(2));
+		return fees;
+	} 
 }
