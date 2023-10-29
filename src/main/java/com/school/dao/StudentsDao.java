b3bb1786bd5ce5fc7a3bccf9574b830bcbef0aa0
RICHA JHA <jricha283@gmail.com>
2023-10-29 16:35:56 +0530

    StudentsDao interface with CRUD operations.

--- src/main/java/com/school/dao/StudentsDao.java
+++ src/main/java/com/school/dao/StudentsDao.java
@@ -1,5 +1,15 @@
 package com.school.dao;
 
-public class StudentsDao {
+import java.util.List;
 
+import com.school.dto.AdmissionDto;
+import com.school.dto.StudentsDTO;
+
+public interface StudentsDao{
+
+	public void addStudents(StudentsDTO dto); 
+	public List<AdmissionDto> listStudents();
+	void deleteStudent(String rollNo);
+	void updateStudent(StudentsDTO dto);
+	AdmissionDto getStudent(int id);
 }
