dc2f839e7d62a4e5f94cfe0ca5d7127b1eaf7281
RICHA JHA <jricha283@gmail.com>
2023-10-30 14:01:03 +0530

    Security Initilizer class

--- src/main/java/com/school/config/SecurityInitializer.java
+++ src/main/java/com/school/config/SecurityInitializer.java
@@ -1,5 +1,11 @@
 package com.school.config;
 
-public class SecurityInitializer {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+//This class will register all Filters to our app
+public class SecurityInitializer  extends AbstractSecurityWebApplicationInitializer{
+	
 
 }
